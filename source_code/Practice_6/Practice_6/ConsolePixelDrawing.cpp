417377c8223d54848577d2f8074648de5aa96169
김민관 <alsrhks2508@naver.com>
2022-10-14 13:53:18 +0900

    practice 6

--- source_code/Practice_6/Practice_6/ConsolePixelDrawing.cpp
+++ source_code/Practice_6/Practice_6/ConsolePixelDrawing.cpp
@@ -4,10 +4,10 @@ ConsolePixelFrame::ConsolePixelFrame(int px, int py)
 {
 	console = GetConsoleWindow();
 	console_DC = GetDC(console);
-	pShapes = new Shape * [MAX_NUM_SHAPES];
+	pShapes = new Shape * [MAX_NUM_SHAPES]; //allocate memory by maximun size
 	num_shapes = 0;
-	capacity = MAX_NUM_SHAPES;
-	cpfr_px = px;
+	capacity = MAX_NUM_SHAPES; //capacity is 100
+	cpfr_px = px; // set coordinate
 	cpfr_py = py;
 }
 ConsolePixelFrame::~ConsolePixelFrame()
@@ -18,27 +18,28 @@ ConsolePixelFrame::~ConsolePixelFrame()
 
 void ConsolePixelFrame::addShape(Shape* new_shape)
 {
-	if (num_shapes >= capacity)
+	if (num_shapes >= capacity) //if capacity is insufficient
 	{
 		cout << "ConsolePixelFrame::addShape ==> Expanding capacity to "
 			<< capacity * 2 << "shapes " << endl;
-		Shape** old_shapes = pShapes;
-		pShapes = new Shape * [capacity * 2];
-		if (pShapes == NULL)
+		Shape** old_shapes = pShapes; //get the allocate memory
+		pShapes = new Shape * [capacity * 2]; //get double sized allocate memory
+		if (pShapes == NULL) //if you are in memory exhaustion 
 		{
 			cout << "Error in expanding dynamic array for shapes capacity "
 				<< capacity * 2 << "shapes " << endl;
-			exit;
+			exit; //exit this program
 		}
-		for (int i = 0; i < num_shapes; i++)
+		for (int i = 0; i < num_shapes; i++) 
 		{
-			pShapes[i] = old_shapes[i];
+			pShapes[i] = old_shapes[i]; //copy them
 		}
-		capacity = capacity * 2;
+		capacity = capacity * 2; 
 		delete[] old_shapes;
 	}
-	pShapes[num_shapes] = new_shape;
-	num_shapes++;
+	//or
+	pShapes[num_shapes] = new_shape; //new shape saved as reference
+	num_shapes++;// prepare next shape
 }
 
 void ConsolePixelFrame::drawShapes()
@@ -46,8 +47,8 @@ void ConsolePixelFrame::drawShapes()
 	cout << "Drawing " << num_shapes << " shapes :" << endl;
 	if (num_shapes > 0)
 	{
-		for (int i = 0; i < num_shapes; i++)
-			pShapes[i]->draw(*this);
+		for (int i = 0; i < num_shapes; i++) //for each shapes
+			pShapes[i]->draw(*this); //using pointer, the draw function execution
 	}
 }
 
